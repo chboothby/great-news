e1b8545dc8f7a2f53d8d435b63c30fc5f538eca7
Clara <clarahboothby@gmail.com>
2020-12-07 17:26:19 +0000

    added voting bar to individual article

--- src/Components/Article.jsx
+++ src/Components/Article.jsx
@@ -1,4 +1,5 @@
 import { getArticleById } from "./api";
+import Voting from "./Voting";
 
 const { useState, useEffect } = require("react");
 
@@ -9,9 +10,24 @@ function Article({ article_id }) {
     getArticleById(article_id).then((article) => {
       setArticle(article);
     });
-  });
+  }, [article_id]);
 
-  return <h2>{article.title}</h2>;
+  return (
+    <section className="articlePage">
+      <div className="articleMain">
+        {" "}
+        <h2>{article.title}</h2>
+        <h3>{article.author} </h3>
+        <h4>{new Date(article.created_at).toDateString()}</h4>
+        <p>{article.body}</p>
+      </div>
+      <div classNae="articleMetrics">
+        <p>comments: {article.comment_count}</p>
+        <p>votes: {article.votes}</p>
+      </div>
+      <Voting />
+    </section>
+  );
 }
 
 export default Article;
---
+++ src/Components/Voting.jsx
@@ -0,0 +1,12 @@
+function Voting() {
+  return (
+    <section className="votingBar">
+      <button>Superlike</button>
+      <button>Like</button>
+
+      <button>Dislike</button>
+    </section>
+  );
+}
+
+export default Voting;
