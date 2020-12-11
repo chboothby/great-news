772a13f46623b45caa61c227c49a2b34d31f5ce5
Clara <clarahboothby@gmail.com>
2020-12-11 14:01:13 +0000

    added nav bar toggle for smaller screen sizes

--- src/Components/CommentCard.jsx
+++ src/Components/CommentCard.jsx
@@ -4,6 +4,7 @@ import styles from "../Styles/Comments.module.css";
 import CommentVotes from "./CommentVotes";
 import { useContext } from "react";
 import { UserContext } from "../App";
+import Popup from "reactjs-popup";
 
 function CommentCard({ comments, deleteComment }) {
   const { username } = useContext(UserContext);
@@ -27,12 +28,30 @@ function CommentCard({ comments, deleteComment }) {
               </p>
               <p>{moment(`${comment.created_at}`, "YYYYMMDDHH").fromNow()}</p>
               {comment.author === username ? (
-                <button
-                  onClick={() => handleClick(comment.comment_id)}
-                  className={styles.deleteBtn}
+                <Popup
+                  trigger={
+                    <button className={styles.deleteBtn}>
+                      {" "}
+                      Delete your comment{" "}
+                    </button>
+                  }
+                  modal
+                  nested
                 >
-                  Delete your comment
-                </button>
+                  {(close) => (
+                    <div className={styles.modal}>
+                      <button className={styles.close} onClick={close}>
+                        &times;
+                      </button>
+                      <div className={styles.header}> Are you sure? </div>
+                      <div className={styles.content}>
+                        <button onClick={() => handleClick(comment.comment_id)}>
+                          Yes, delete
+                        </button>
+                      </div>
+                    </div>
+                  )}
+                </Popup>
               ) : null}
             </div>
             <CommentVotes comment={comment} />
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -2,34 +2,44 @@ import { Link } from "@reach/router";
 import { useEffect, useState } from "react";
 import { getTopics } from "../api";
 import styles from "../Styles/Nav.module.css";
-import { FiHome } from "react-icons/fi";
+import { FiHome, FiMenu } from "react-icons/fi";
+import { ImCross } from "react-icons/im";
 
 function Nav() {
   const [topics, setTopics] = useState([]);
-
+  const [isToggled, setToggle] = useState(false);
   useEffect(() => {
     getTopics().then((response) => {
       setTopics(response);
     });
   }, []);
 
+  const toggleNav = () => {
+    isToggled ? setToggle(false) : setToggle(true);
+  };
   return (
     <nav className={styles.navBar}>
       <Link to="/" className={styles.recent}>
         <FiHome />
       </Link>
-      <section className={styles.topics}>
-        {topics.map((topic) => {
-          return (
-            <Link
-              to={`/articles/topics/${topic.slug}`}
-              className={styles.topic}
-              key={`${topic.slug}`}
-            >
-              {topic.slug}
-            </Link>
-          );
-        })}
+      <section className={styles.menu}>
+        <button onClick={toggleNav} className={styles.toggle}>
+          {isToggled ? <ImCross /> : <FiMenu />}
+        </button>
+        <div className={isToggled ? styles.isToggled : styles.topics}>
+          {topics.map((topic) => {
+            return (
+              <Link
+                onClick={toggleNav}
+                to={`/articles/topics/${topic.slug}`}
+                className={styles.topic}
+                key={`${topic.slug}`}
+              >
+                {topic.slug}
+              </Link>
+            );
+          })}
+        </div>
       </section>
     </nav>
   );
